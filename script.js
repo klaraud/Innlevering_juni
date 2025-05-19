6d20c0ec9d1e13b4a1e83650dbe5b2c33f673688
Klara <klarauvaagdanielsen@gmail.com>
2025-05-19 09:13:31 +0200

    alt hittil

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  
+// konstant tyngdefart
+let GRAVITASJON = 1
+
+
+
+// Index 
+
+const imgElm = document.getElementById("historieImg")
+const textElm = document.getElementById("historieText")
+
+
+  
